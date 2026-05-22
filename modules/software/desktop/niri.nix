520f7e49e70305576bfaaf389f6b38755bf87c25
int16 <int16int8@gmail.com>
2026-05-22 17:17:20 +0800

    feat: init add niri

---
+++ modules/software/desktop/niri.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+{
+  # Wayland Desktop Environment Configuations.
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      niri_session = {
+        command = "${config.programs.niri.package}/bin/niri-session";
+        user = "myuser";
+      };
+    };
+  };
+
+  systemd.user.services.niri.enableDefaultPath = false;
+}
