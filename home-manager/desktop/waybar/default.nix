3391bd237db9861ddfb26059ea476361a01d80e0
int16 <int16int8@gmail.com>
2026-05-29 17:26:28 +0800

    feat: update waybar style

--- home-manager/desktop/waybar/default.nix
+++ home-manager/desktop/waybar/default.nix
@@ -1,6 +1,9 @@
-{pkgs,config,...}:{
+{ pkgs, config, ... }: {
   programs.waybar.enable = true;
-  
+
+  # Fcitx5 tray icon uses input-keyboard-symbolic from Adwaita.
+  home.packages = [ pkgs.adwaita-icon-theme ];
+
   xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
   xdg.configFile."waybar/style.css".source = ./style.css;
 }
