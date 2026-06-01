0a3452a51ea5af1969520015a5cf2e888ece7726
int16 <int16int8@gmail.com>
2026-06-01 11:56:41 +0800

    feat: update niri config

--- home-manager/desktop/niri/default.nix
+++ home-manager/desktop/niri/default.nix
@@ -3,6 +3,7 @@
     imports = [
         ../waybar
         ../wofi
+        ../wlogout
     ];
 
     programs.alacritty.enable = true; # Super+T in the default setting (terminal)
@@ -17,13 +18,17 @@
         xwayland-satellite # xwayland support
         wl-clipboard
         bibata-cursors
+
+        networkmanagerapplet
+        brightnessctl
     ];
 
-    xdg.configFile."niri/config.kdl".source = ./niri.kdl;
-    xdg.configFile."niri/input.kdl".source = ./input.kdl;
-    xdg.configFile."niri/output.kdl".source = ./output.kdl;
-    xdg.configFile."niri/bind.kdl".source = ./bind.kdl;
-    xdg.configFile."niri/layout.kdl".source = ./layout.kdl;
-    xdg.configFile."niri/startup.kdl".source = ./startup.kdl;
-    xdg.configFile."niri/windowrule.kdl".source = ./windowrule.kdl;
+    xdg.configFile."niri/config.kdl".source = ./config/niri.kdl;
+    xdg.configFile."niri/input.kdl".source = ./config/input.kdl;
+    xdg.configFile."niri/output.kdl".source = ./config/output.kdl;
+    xdg.configFile."niri/bind.kdl".source = ./config/bind.kdl;
+    xdg.configFile."niri/layout.kdl".source = ./config/layout.kdl;
+    xdg.configFile."niri/startup.kdl".source = ./config/startup.kdl;
+    xdg.configFile."niri/windowrule.kdl".source = ./config/windowrule.kdl;
+    xdg.configFile."niri/misc.kdl".source = ./config/misc.kdl;
 }
