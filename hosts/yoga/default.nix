32eac7ba3001d14831110038cbf24dc3eaa334e3
int16 <int16int8@gmail.com>
2026-06-22 15:39:54 +0800

    feat: fix audio for yoga air 14s

--- hosts/yoga/default.nix
+++ hosts/yoga/default.nix
@@ -1,4 +1,19 @@
 { config, pkgs, lib, username, ... }:
+let
+  yogaUcmOverride = pkgs.writeText "LENOVO-83ED-YOGAAir14sQ8X9-LNVNB161216.conf" ''
+    Syntax 4
+
+    Include.slim7x.File "/Qualcomm/x1e80100/LENOVO-Slim-7x.conf"
+  '';
+
+  alsaUcm2 = pkgs.symlinkJoin {
+    name = "alsa-ucm2-yoga";
+    paths = [ pkgs.alsa-ucm-conf ];
+    postBuild = ''
+      install -Dm644 ${yogaUcmOverride} $out/share/alsa/ucm2/conf.d/x1e80100/LENOVO-83ED-YOGAAir14sQ8X9-LNVNB161216.conf
+    '';
+  };
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -21,6 +36,17 @@
   nixpkgs.hostPlatform.system = "aarch64-linux";
 
   hardware.lenovo-yoga-slim7x.enable = true;
+
+  # Card long name does not match x1e80100.conf DMI regex ("Yoga Slim 7" vs
+  # product_family "YOGA Air 14s Q8X9"). Merge override into the full UCM2 tree.
+  environment.etc."alsa/ucm2".source = lib.mkForce "${alsaUcm2}/share/alsa/ucm2";
+
+  # NixOS alsa-lib does not pick up /etc/alsa/ucm2 on its own; PipeWire ACP
+  # needs this to load the Yoga profile instead of falling back to null sink.
+  environment.sessionVariables.ALSA_CONFIG_UCM2 = "/etc/alsa/ucm2";
+  systemd.user.services.pipewire.environment.ALSA_CONFIG_UCM2 = "/etc/alsa/ucm2";
+  systemd.user.services.pipewire-pulse.environment.ALSA_CONFIG_UCM2 = "/etc/alsa/ucm2";
+  systemd.user.services.wireplumber.environment.ALSA_CONFIG_UCM2 = "/etc/alsa/ucm2";
 
   boot.loader.systemd-boot.configurationLimit = 4;
 
