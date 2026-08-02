f821fd90120917fb7b2c6d1ef5cc375508032f98
int16 <int16int8@gmail.com>
2026-08-02 19:54:05 +0800

    feat: update steam config

--- modules/software/game/steam.nix
+++ modules/software/game/steam.nix
@@ -5,7 +5,30 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+    gamescopeSession.enable = true;
+
+    package = pkgs.steam.override {
+      extraPkgs = pkgs': with pkgs'; [
+        libXcursor
+        libXi
+        libXinerama
+        libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib # Provides libstdc++.so.6
+        libkrb5
+        keyutils
+        # Add other libraries as needed
+      ];
   };
 
-  programs.gamescope.enable = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      enableWsi = true;
+      capSysNice = true;
+    };
+  };
 }
