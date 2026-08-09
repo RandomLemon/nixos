f418d6dd1d66842128e015cb336854aa5b396e72
int16 <int16int8@gmail.com>
2026-08-09 19:56:02 +0800

    fix: steam fail to build on #tx

--- modules/software/game/steam.nix
+++ modules/software/game/steam.nix
@@ -7,22 +7,6 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
 
     gamescopeSession.enable = true;
-
-    package = pkgs.steam.override {
-      extraPkgs = pkgs': with pkgs'; [
-        libXcursor
-        libXi
-        libXinerama
-        libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib # Provides libstdc++.so.6
-        libkrb5
-        keyutils
-        # Add other libraries as needed
-      ];
-    };
   };
 
   programs = {
